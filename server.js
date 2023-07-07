587cb383c82003fc96ce44d008f1776db7796e5a
Nenad T <trajkovik.nenad@gmail.com>
2023-07-07 13:52:34 +0200

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    res.json({ success: "Connected" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
